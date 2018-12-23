06db30d710f6f1277a041cc3f5e2e82f6198c1c0
Justin Holguin <juxtin@github.com>
2018-12-22 22:55:44 -0800

    Slight test improvements

--- test/smoke_test.go
+++ test/smoke_test.go
@@ -4,15 +4,14 @@ import (
 	"testing"
 
 	"github.com/juxtin/gore/pkg/gore"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestRun(t *testing.T) {
+func TestImportsInFile(t *testing.T) {
 	inputs := []string{"../pkg/main/main.go"}
 	expected := []string{"fmt", "github.com/juxtin/gore/pkg/gore", "os"}
 	results, err := gore.ImportsInFile(inputs)
-	if err != nil {
-		require.Fail(t, "Error during test!", err)
-	}
-	require.Equal(t, expected, results)
+	require.Nil(t, err, "Error reading the test input file!")
+	assert.Equal(t, expected, results)
 }
