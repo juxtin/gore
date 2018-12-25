9db02d1e48f613f795a307294a30dcfb16a354f8
Justin Holguin <juxtin@github.com>
2018-12-24 16:31:30 -0800

    Add real test assertions for the graph package

--- script/test.sh
+++ script/test.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 
 # this is bad and wrong, but it works for now
 go test test/smoke/smoke_test.go
+go test test/graph/graph_test.go
--- test/graph/graph_test.go
+++ test/graph/graph_test.go
@@ -1,9 +1,11 @@
 package graph
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/juxtin/gore/pkg/graph"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGraph(t *testing.T) {
@@ -15,6 +17,50 @@ func TestGraph(t *testing.T) {
 	g.AddImport("parse", "util")
 	g.AddImport("net", "tcp")
 	g.AddImport("net", "util")
-	g.Graphviz()
-	// no assertion here for now, I'll figure something out
+	s := g.Graphviz()
+	// The order of lines returned by gographviz is non-deterministic, so we
+	// need to test it by set comparison.
+	results := newStringSet()
+	for _, line := range strings.Split(s, "\n") {
+		results.add(strings.TrimSpace(line))
+	}
+	expected := []string{
+		"main->util;",
+		"main->net;",
+		"main->cli;",
+		"cli->parse;",
+		"net->tcp;",
+		"net->util;",
+		"cli;",
+		"main;",
+		"net;",
+		"parse;",
+		"tcp;",
+		"util;",
+	}
+	for _, v := range expected {
+		assertContains(t, results, v)
+	}
+}
+
+// This language wisely lacks generics, which gives me the exciting opportunity
+// to re-implement sets on an ad-hoc basis
+type stringSet map[string]bool
+
+func assertContains(t *testing.T, s stringSet, k string) {
+	assert.Equal(t, s.contains(k), true, "Result did not contain '"+k+"'")
+}
+
+func newStringSet() stringSet {
+	return make(map[string]bool)
+}
+
+func (s stringSet) add(k string) stringSet {
+	s[k] = true
+	return s
+}
+
+func (s stringSet) contains(key string) bool {
+	_, ok := s[key]
+	return ok
 }
