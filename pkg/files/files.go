a16d80618ab238eca811889a70d23acdea502f3f
Justin Holguin <juxtin@github.com>
2018-12-24 23:17:13 -0800

    Initial version of files package

---
+++ pkg/files/files.go
@@ -0,0 +1,38 @@
+package files
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+type FS struct {
+	gopath string
+	Files  map[string][]byte
+}
+
+func NewFS(gopath string) *FS {
+	return &FS{gopath, map[string][]byte{}}
+}
+
+func isFullyQualified(path string) bool {
+	return strings.Contains(path, "/")
+}
+
+func (fs *FS) Resolve(path string) string {
+	if isFullyQualified(path) {
+		return fs.gopath + "/src/" + path
+	}
+	return path
+}
+
+func (fs *FS) CacheFile(path string) {
+	if _, ok := fs.Files[path]; ok {
+		return
+	}
+	document, err := ioutil.ReadFile(path)
+	if err != nil {
+		fs.Files[path] = nil
+	} else {
+		fs.Files[path] = document
+	}
+}
