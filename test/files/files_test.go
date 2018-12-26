59c420c74a7d433c8983fec9e0f1adb9c4f043d1
Justin Holguin <juxtin@github.com>
2018-12-25 16:18:32 -0800

    Add test for files package

---
+++ test/files/files_test.go
@@ -0,0 +1,19 @@
+package files
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/juxtin/gore/pkg/files"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFiles(t *testing.T) {
+	gopath := os.Getenv("GOPATH")
+	fs := files.NewFS(gopath)
+	exFile := string(fs.CacheFile("github.com/juxtin/gore/pkg/files/files.go"))
+	fmt.Println("File:", exFile)
+	assert.True(t, strings.Contains(exFile, "package files"), exFile)
+}
