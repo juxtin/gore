d2025ee8050e58194c0eeaebe63658f91da53190
Justin Holguin <juxtin@github.com>
2018-12-27 13:38:56 -0800

    Put a tiny bit of polish on to make it usable

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -9,13 +9,28 @@ import (
 	"github.com/juxtin/gore/pkg/gore"
 )
 
+func die(msg string) {
+	fmt.Fprintf(os.Stderr, msg+"\n")
+	os.Exit(1)
+}
+
+func usage() {
+	die("Usage:\n\tgore path/to/project/root")
+}
+
 func main() {
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		die("GOPATH must be set!")
+	}
+	if len(os.Args) != 2 {
+		usage()
+	}
 	arg := os.Args[1]
 	rootDir, err := filepath.Abs(arg)
 	if err != nil {
-		fmt.Println("Oops:", err)
+		die("Unable to find absolute path to '" + arg + "': " + err.Error())
 	}
 	debug.Print("Root dir:", rootDir)
-	gopath := os.Getenv("GOPATH")
 	gore.Smoke(gopath, rootDir)
 }
