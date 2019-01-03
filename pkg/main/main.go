4b01645cafcf172956c24aaa63043ed58029ded2
Justin Holguin <juxtin@github.com>
2019-01-03 14:52:22 -0800

    Initial cut at proper CLI

--- pkg/main/main.go
+++ pkg/main/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/juxtin/gore/pkg/debug"
 	"github.com/juxtin/gore/pkg/gore"
+	cli "gopkg.in/urfave/cli.v1"
 )
 
 func die(msg string) {
@@ -14,23 +15,56 @@ func die(msg string) {
 	os.Exit(1)
 }
 
-func usage() {
-	die("Usage:\n\tgore path/to/project/root")
+func graph(c *cli.Context) error {
+	rootDirArg := c.String("dir")
+	gopath := c.String("gopath")
+	if len(gopath) == 0 {
+		die("You must either set $GOPATH in your shell or use the --gopath flag")
+	}
+	debug.Print("Gopath:", gopath)
+	output := c.String("output")
+	debug.Print("Root Dir:", rootDirArg)
+	debug.Print("Output:", output)
+	rootDir, err := filepath.Abs(rootDirArg)
+	if err != nil {
+		die("Unable to find absolute path to '" + rootDirArg + "': " + err.Error())
+	}
+	graph := gore.BuildGraph(gopath, rootDir)
+	if len(output) == 0 {
+		fmt.Println(graph)
+	} else {
+		fmt.Println("writing to", output)
+		die("not implemented yet!")
+	}
+	return nil
 }
 
 func main() {
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		die("GOPATH must be set!")
+	app := cli.NewApp()
+	app.Name = "gore"
+	app.Usage = "Visualize a Golang project's internal import graph"
+	app.UsageText = "gore [global options] [root directory]"
+	app.Version = "0.0.1"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "gopath, g",
+			Usage:  "GOPATH to use while scanning for files",
+			EnvVar: "GOPATH",
+		},
+		cli.StringFlag{
+			Name:  "dir, d",
+			Usage: "directory under which the .go source files are located",
+			Value: ".",
+		},
+		cli.StringFlag{
+			Name:  "output, o",
+			Usage: "destination filepath for the output graph (default: stdout)",
+		},
 	}
-	if len(os.Args) != 2 {
-		usage()
-	}
-	arg := os.Args[1]
-	rootDir, err := filepath.Abs(arg)
+	app.Action = graph
+
+	err := app.Run(os.Args)
 	if err != nil {
-		die("Unable to find absolute path to '" + arg + "': " + err.Error())
+		die(err.Error())
 	}
-	debug.Print("Root dir:", rootDir)
-	gore.Smoke(gopath, rootDir)
 }
